bab56c08a4676390c9b87afbb2e82aaf871e477f
Guilherme Mello <gui.scs@gmail.com>
2021-01-21 01:32:12 -0300

    Ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from  './views/home/home.component'
+// import {HomeComponent} from  './views/home/home.component'
 import  {ProductCrudComponent} from'./views/product-crud/product-crud.component'
 import { LoginComponent } from './views/login/login.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
@@ -13,10 +13,10 @@ const routes: Routes = [
     path:"",
     component:LoginComponent
   },
-  {
-  path:"home",
-  component:HomeComponent
-  },
+  //{
+  // path:"home",
+  // component:HomeComponent
+  // },
   {
     path:"products",
     component: ProductCrudComponent
